4d291dd3b444c7430cc97f617bb61fe647de778c
Povilas Jurgulis <povilas.jurgulis@mif.stud.vu.lt>
2025-09-20 15:09:59 +0300

    sukūriau main.h, kuriame įdėjau svarbias bibliotekas

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
-#include <unordered_map>
+#include <main.h>
 
 /* Povilo Jurgulio 1 gr. 1 pogr. VU ISI 1 laboratorinis Block Chain darbas */
 int main()
 {
-    std::cout << "Hello World!";
+    cout << "Hello World!";
 }
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream> // cout ir t.t.
+#include <unordered_map> // hash
+#include <chrono> // laikui
+#include <random> // rng
+#include <string> // string
+
+using std::unordered_map; using std::cout; using std::ifstream; using std::endl;
+using namespace std::chrono; using std::random_device; using std::mt19937;
+
+#endif
